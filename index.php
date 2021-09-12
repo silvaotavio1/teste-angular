c408a35125b28bee854760668593fa1e9dcd85de
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 13:47:10 -0300

    no message

--- index.php
+++ index.php
@@ -16,9 +16,9 @@
 <body ng-app="copyExample">
   <div ng-controller="ExampleController">
     <form novalidate class="simple-form">
-      <label>CPF Indica: <input type="text" ng-model="user.cpf_indica" /></label><br />
-      <label>CPF Indicado: <input type="text" ng-model="user.cpf_indicado" /></label><br />
-      <label>Email Indicado: <input type="text" ng-model="user.email_indicado" /></label><br />
+      <label>CPF Indica: <input type="text" ng-model="user.cpf_indica" /></label><br><br>
+      <label>CPF Indicado: <input type="text" ng-model="user.cpf_indicado" /></label><br><br>
+      <label>Email Indicado: <input type="text" ng-model="user.email_indicado" /></label><br><br>
 
       <button ng-click="reset()">Limpar</button>
       <button ng-click="update(user)">Salvar</button>
