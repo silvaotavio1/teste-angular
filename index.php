050a47c08ee78422a92c7997427aebebf2aaf6f0
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 13:22:53 -0300

    no message

--- index.php
+++ index.php
@@ -1,17 +1,12 @@
-<!doctype html>
-<html>
-<body>
-<div ng-controller="WelcomeController">
-  {{greeting}}
+<div ng-controller="ExampleController">
+  <form novalidate class="simple-form">
+    <label>Name: <input type="text" ng-model="user.name" /></label><br />
+    <label>Age:  <input type="number" ng-model="user.age" /></label><br />
+    Gender: <label><input type="radio" ng-model="user.gender" value="male" />male</label>
+            <label><input type="radio" ng-model="user.gender" value="female" />female</label><br />
+    <button ng-click="reset()">RESET</button>
+    <button ng-click="update(user)">SAVE</button>
+  </form>
+  <pre>form = {{user | json}}</pre>
+  <pre>leader = {{leader | json}}</pre>
 </div>
-
-<script src="angular.js"></script>
-<script>
-  var app = angular.module('demo', [])
-  .controller('WelcomeController', function($scope) {
-      $scope.greeting = 'Welcome!';
-  });
-  angular.bootstrap(document, ['demo']);
-</script>
-</body>
-</html>
