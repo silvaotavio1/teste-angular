d6b43f8148a4a8c47d86f7115d01d7739df1ac9b
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 13:57:48 -0300

    no message

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 angular.
   module('copyExample', []).
   controller('ExampleController', ['$scope', function($scope) {
-    $scope.submit = {};
+    $scope.indicacao = {};
 
     $scope.reset = function() {
       $scope.indicacao = {}
     };
 
     $scope.update = function(user) {
-      $scope.submit = angular.copy(indicacao);
+      $scope.indicacao = angular.copy(indicacao);
     };
 
     //$scope.reset();
