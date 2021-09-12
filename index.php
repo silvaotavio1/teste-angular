18510ead26432485c109d87f82a679ecb2565400
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 13:46:13 -0300

    no message

--- index.php
+++ index.php
@@ -1,30 +1,33 @@
 <!doctype html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <title>Teste Angular - Otávio</title>
-  
+
 
   <script src="//code.angularjs.org/1.8.2/angular.min.js"></script>
   <script src="script.js"></script>
-  
 
-  
+
+
 </head>
+
 <body ng-app="copyExample">
   <div ng-controller="ExampleController">
-  <form novalidate class="simple-form">
-    <label>Name: <input type="text" ng-model="user.name" /></label><br />
-    <label>Age:  <input type="number" ng-model="user.age" /></label><br />
-    Gender: <label><input type="radio" ng-model="user.gender" value="male" />male</label>
-            <label><input type="radio" ng-model="user.gender" value="female" />female</label><br />
-    <button ng-click="reset()">RESET</button>
-    <button ng-click="update(user)">SAVE</button>
-  </form>
-  <pre>form = {{user | json}}</pre>
-  <pre>leader = {{leader | json}}</pre>
-</div>
+    <form novalidate class="simple-form">
+      <label>CPF Indica: <input type="text" ng-model="user.cpf_indica" /></label><br />
+      <label>CPF Indicado: <input type="text" ng-model="user.cpf_indicado" /></label><br />
+      <label>Email Indicado: <input type="text" ng-model="user.email_indicado" /></label><br />
+
+      <button ng-click="reset()">Limpar</button>
+      <button ng-click="update(user)">Salvar</button>
+    </form>
+    <pre>form = {{user | json}}</pre>
+    <pre>leader = {{leader | json}}</pre>
+  </div>
 </body>
+
 </html>
 
 <!-- 
