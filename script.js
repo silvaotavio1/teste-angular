965806fc1a6cec77530eec636482efc2d1bc6be8
silvaotavio1 <otaviosilva96@gmail.com>
2021-09-12 17:29:11 -0300

    Att final

--- script.js
+++ script.js
@@ -6,116 +6,33 @@
     controller('ExampleController', ['$scope', function ($scope) {
       $scope.indicacao = {};
 
+      //Limpar campos [0]
       $scope.reset = function () {
-        // Example with 1 argument
-        testeAlerta()
         $scope.indicacao = {};
         document.getElementById('pre-response').innerHTML = 'response = {}'
       };
 
+      //Listar todas as indicações [1]
+      $scope.listar = function () {
+        requestAPI('GET', 'indicacoes')
+      };
+
+      //Criar uma nova indicação [2]
+      $scope.update = function (indicacao) {
+        var raw = JSON.stringify(angular.copy(indicacao))
+        requestAPI('POST', 'indicacao', raw)
+      };
+
+      //Deletar uma indicação vigente [3]
       $scope.deletar = function () {
         var codindicacao = prompt("Atenção, insira o codigo da indicação que deseja excluir: ", "");
-        
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer 9f10a9cbc46a4faebcd8e904d1c24d25");
-        myHeaders.append("Content-Type", "application/json");
-
-        var requestOptions = {
-          method: 'DELETE',
-          headers: myHeaders,
-          redirect: 'follow'
-        };
-
-        fetch("https://www.quadrosdaroberta.com.br/laravelphp/api/indicacao/" + codindicacao, requestOptions)
-          .then(response => response.text())
-          .then(result => {
-            console.log(result);
-            document.getElementById('pre-response').innerHTML = 'response = ' + JSON.stringify(JSON.parse(result), null, 2);
-          }).catch(error => {
-            console.log('error', error)
-            document.getElementById('pre-response').innerHTML = 'response = Não é possível acessar essa informação'
-          });
-
-        document.getElementById('pre-response').innerHTML = 'response = Loading...';
+        requestAPI('DELETE', 'avancastatus', null, codindicacao)
       };
 
+      //Avançar Status da indicação [4]
       $scope.avancar = function () {
-        var codindicacao = prompt("Atenção, insira o codigo da indicação que deseja avançar o status: ", "");
-        
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer 9f10a9cbc46a4faebcd8e904d1c24d25");
-        myHeaders.append("Content-Type", "application/json");
-
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          redirect: 'follow'
-        };
-
-        fetch("https://www.quadrosdaroberta.com.br/laravelphp/api/avancastatus/" + codindicacao, requestOptions)
-          .then(response => response.text())
-          .then(result => {
-            console.log(result);
-            document.getElementById('pre-response').innerHTML = 'response = ' + JSON.stringify(JSON.parse(result), null, 2);
-          }).catch(error => {
-            console.log('error', error)
-            document.getElementById('pre-response').innerHTML = 'response = Não é possível acessar essa informação'
-          });
-
-        document.getElementById('pre-response').innerHTML = 'response = Loading...';
-      };
-
-      $scope.listar = function () {
-
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer 9f10a9cbc46a4faebcd8e904d1c24d25");
-        myHeaders.append("Content-Type", "application/json");
-
-        var requestOptions = {
-          method: 'GET',
-          headers: myHeaders,
-          redirect: 'follow'
-        };
-
-        fetch("https://www.quadrosdaroberta.com.br/laravelphp/api/indicacoes/", requestOptions)
-          .then(response => response.text())
-          .then(result => {
-            console.log(result);
-            document.getElementById('pre-response').innerHTML = 'response = ' + JSON.stringify(JSON.parse(result), null, 2);
-          }).catch(error => {
-            console.log('error', error)
-            document.getElementById('pre-response').innerHTML = 'response = Não é possível acessar essa informação'
-          });
-
-        document.getElementById('pre-response').innerHTML = 'response = Loading...';
-      };
-
-      $scope.update = function (indicacao) {
-
-        var myHeaders = new Headers();
-        myHeaders.append("Authorization", "Bearer 9f10a9cbc46a4faebcd8e904d1c24d25");
-        myHeaders.append("Content-Type", "application/json");
-
-        var raw = JSON.stringify(angular.copy(indicacao));
-
-        var requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw,
-          redirect: 'follow'
-        };
-
-        fetch("https://www.quadrosdaroberta.com.br/laravelphp/api/indicacao/", requestOptions)
-          .then(response => response.text())
-          .then(result => {
-            console.log(result);
-            document.getElementById('pre-response').innerHTML = 'response = ' + JSON.stringify(JSON.parse(result), null, 2);
-          }).catch(error => {
-            console.log('error', error)
-            document.getElementById('pre-response').innerHTML = 'response = Não é possível acessar essa informação'
-          });
-
-        document.getElementById('pre-response').innerHTML = 'response = Loading...';
+        var codindicacao = prompt("Atenção, insira o codigo da indicação que deseja excluir: ", "");
+        requestAPI('POST', 'avancastatus', null, codindicacao)
       };
 
       $scope.reset();
@@ -124,7 +41,27 @@
 
 
 
-function testeAlerta()
-{
-  alert('oi');
+function requestAPI(type, rota, raw = null, codindicacao = '') {
+  var myHeaders = new Headers();
+  myHeaders.append("Authorization", "Bearer 9f10a9cbc46a4faebcd8e904d1c24d25");
+  myHeaders.append("Content-Type", "application/json");
+
+  var requestOptions = {
+    method: type,
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+  };
+
+  fetch("https://www.quadrosdaroberta.com.br/laravelphp/api/" + rota + '/' + codindicacao, requestOptions)
+    .then(response => response.text())
+    .then(result => {
+      console.log(result);
+      document.getElementById('pre-response').innerHTML = 'response = ' + JSON.stringify(JSON.parse(result), null, 2);
+    }).catch(error => {
+      console.log('error', error)
+      document.getElementById('pre-response').innerHTML = 'response = Não é possível acessar essa informação'
+    });
+
+  document.getElementById('pre-response').innerHTML = 'response = Loading...';
 }
